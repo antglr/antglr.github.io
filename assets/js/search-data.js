1cc8c40e2293b78e521848f43107734f49203715
antglr <antonio94gilardi@gmail.com>
2026-01-14 16:42:33 +0000

    Deploying to gh-pages from @ antglr/antglr.github.io@2782b625f5a1c4f29b0d0bb7cc35b5fb34c63b54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,9 +405,9 @@ ninja.data = [{
           description: "ML approach for flattening the beam",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-vesuvionet",
+          title: 'VesuvioNet',
+          description: "An app for emergency endling in Naples",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
